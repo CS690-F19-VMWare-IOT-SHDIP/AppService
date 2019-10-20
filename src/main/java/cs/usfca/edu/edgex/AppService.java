03344f9128a31ed43622ce033f9df67246f53ba8
Kunal Sonar <ksonar@Kunals-MacBook-Pro.local>
2019-10-19 19:41:51 -0700

    added spring dependency

--- src/main/java/cs/usfca/edu/edgex/AppService.java
+++ src/main/java/cs/usfca/edu/edgex/AppService.java
@@ -1,5 +1,8 @@
 package cs.usfca.edu.edgex;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import cs.usfca.edu.edgex.device.BulbDevice;
 import cs.usfca.edu.edgex.device.Device;
 import cs.usfca.edu.edgex.event.Event;
@@ -14,19 +17,21 @@ import cs.usfca.edu.edgex.utils.Node;
  * Driver class for AppService.
  *
  */
+@SpringBootApplication
 public class AppService {
 	public static void main(String args[]) throws InterruptedException {
-		System.out.println("Welcome to IOT workflow!");
-		DeviceModel bulbDeviceModel = new DeviceModel("dummy", "dummy", "dummy");
-		Device<Boolean> bulb = new BulbDevice(bulbDeviceModel);
-		DeviceEvent<Boolean> lightOnEvent = new LightOnEvent(bulb);
-		Event randomModEvent = new RandomModEvent(7);
-		Node child = new Node();
-		child.addEvent(lightOnEvent);
-		Node head = new Node();
-		head.addEvent(randomModEvent);
-		head.addChildren(child);
-		Flow flow = new Flow(head);
-		flow.executeFlow();
+		SpringApplication.run(AppService.class, args);
+//		System.out.println("Welcome to IOT workflow!");
+//		DeviceModel bulbDeviceModel = new DeviceModel("dummy", "dummy", "dummy");
+//		Device<Boolean> bulb = new BulbDevice(bulbDeviceModel);
+//		DeviceEvent<Boolean> lightOnEvent = new LightOnEvent(bulb);
+//		Event randomModEvent = new RandomModEvent(7);
+//		Node child = new Node();
+//		child.addEvent(lightOnEvent);
+//		Node head = new Node();
+//		head.addEvent(randomModEvent);
+//		head.addChildren(child);
+//		Flow flow = new Flow(head);
+//		flow.executeFlow();
 	}
 }
