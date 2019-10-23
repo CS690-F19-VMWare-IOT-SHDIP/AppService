7625bd98cb5c5da8361ef4d2942b6f615d0aaaa9
Kunal Sonar <ksonar@Kunals-MacBook-Pro.local>
2019-10-22 19:09:04 -0700

    event feature API and handlers

---
+++ src/main/java/cs/usfca/edu/edgex/event/LEDOnEvent.java
@@ -0,0 +1,62 @@
+package cs.usfca.edu.edgex.event;
+
+import cs.usfca.edu.edgex.device.Device;
+import cs.usfca.edu.edgex.device.DeviceType;
+import cs.usfca.edu.edgex.exceptions.UnsupportedDeviceTypeException;
+
+public class LEDOnEvent implements Event {
+	
+	private static DeviceType deviceType = DeviceType.LED;
+	private Device<Boolean> led;
+	
+	/**
+	 * Constructor for event with LED device.
+	 * @param led
+	 * @throws UnsupportedDeviceTypeException 
+	 */
+	public LEDOnEvent(Device<Boolean> led) throws UnsupportedDeviceTypeException {
+		if(led.getDeviceType() != deviceType) {
+			throw new UnsupportedDeviceTypeException(led.getDeviceType(), deviceType);
+		}
+		this.led = led;
+	}
+
+	/**
+	 * Checks if the current LED device is switched
+	 * ON or not. It returns true if LED is ON and
+	 * returns false otherwise.
+	 * @param boolean
+	 */
+	public boolean isActive() {
+		return led.get();
+	}
+	
+	/**
+	 * Triggers an event to switch ON the LED.
+	 */
+	public void trigger() {
+		if(!led.get()) {
+			led.set(true); // Triggers LED on.
+		}
+	}
+	
+	/**
+	 * Static method to identify DeviceType that
+	 * this event supports.
+	 * @return Devicetype
+	 */
+	public static DeviceType getDeviceType() {
+		return deviceType;
+	}
+
+	@Override
+	public int compareTo(Event o) {
+		return (this.getDevice().equals(o.getDevice()) ? 0 : 1);
+	}
+
+	@Override
+	public Device<?> getDevice() {
+		return this.led;
+	}
+
+}
